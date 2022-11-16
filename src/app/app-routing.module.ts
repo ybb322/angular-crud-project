8bb1b5ff119b52684f5ade7dda4dc915dd619b17
a_masalevich <a.masalevich@belitsoft.com>
2022-11-17 00:16:29 +0100

    created basic components; added basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuestionPageComponent } from './components/question-page/question-page.component';
+import { AskQuestionComponent } from './components/ask-question/ask-question.component';
+import { QuestionsComponent } from './components/questions/questions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: QuestionsComponent,
+  },
+  {
+    path: 'question/:id',
+    component: QuestionPageComponent,
+  },
+  {
+    path: 'ask-question',
+    component: AskQuestionComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { QuestionsComponent } from './components/questions/questions.component';
+import { QuestionComponent } from './components/question/question.component';
+import { QuestionPageComponent } from './components/question-page/question-page.component';
+import { AskQuestionComponent } from './components/ask-question/ask-question.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+
+    QuestionsComponent,
+    QuestionComponent,
+    QuestionPageComponent,
+    AskQuestionComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
