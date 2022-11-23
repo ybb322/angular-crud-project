1f436c7b1e9ee826b2c6aa0fb59d99a3a58a0ca9
a_masalevich <a.masalevich@belitsoft.com>
2022-11-23 01:00:44 +0100

    added guards

---
+++ src/app/guards/isAuth.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class IsAuthGuard implements CanActivate {
+  constructor(private router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    const user = JSON.parse(localStorage.getItem('user')!);
+    if (!user) {
+      this.router.navigate(['/login']);
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
