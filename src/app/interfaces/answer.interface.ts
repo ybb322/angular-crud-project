4618980e6971adbfd62e65a64c101bc80b52574f
a_masalevich <a.masalevich@belitsoft.com>
2022-11-24 19:48:40 +0100

    added 2 services

---
+++ src/app/interfaces/answer.interface.ts
@@ -0,0 +1,8 @@
+export interface IAnswer {
+  key: string;
+  value: {
+    author: string;
+    authorId: string;
+    body: string;
+  };
+}
---
+++ src/app/interfaces/question.interface.ts
@@ -0,0 +1,7 @@
+export interface IQuestion {
+  answers?: {};
+  author: string;
+  authorId: string;
+  description: string;
+  title: string;
+}
