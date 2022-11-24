19d37af12bbd1c27b9fc34337d885806d70af6eb
a_masalevich <a.masalevich@belitsoft.com>
2022-11-24 07:18:12 +0100

    prod env changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDzsZO5rRzh-xcYip5XJxOXXo3N5JLStxA',
+    authDomain: 'helpdesk-31970.firebaseapp.com',
+    projectId: 'helpdesk-31970',
+    storageBucket: 'helpdesk-31970.appspot.com',
+    messagingSenderId: '636920697580',
+    appId: '1:636920697580:web:80dee5fe8c7070d1f4cbc9',
+  },
 };
